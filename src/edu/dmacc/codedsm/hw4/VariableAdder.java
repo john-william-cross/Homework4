f3ce3af1c122e65a4462cd87366f56b9040ba4c8
John Cross <jwcross1@dmacc.edu>
2019-02-17 16:35:12 -0600

    made instance and constant variable public as directed in slack comment

--- src/edu/dmacc/codedsm/hw4/VariableAdder.java
+++ src/edu/dmacc/codedsm/hw4/VariableAdder.java
@@ -2,8 +2,8 @@ package edu.dmacc.codedsm.hw4;
 
 public class VariableAdder {
 
-    final int FIRST_INT = 1;
-    int secondInt = 2;
+    public final int FIRST_INT = 1;
+    public int secondInt = 2;       //I notice the code still works without making these variables public
 
     public static void main(String[] args) {
 
